927417041f2c0b8427027519dc725af93f849865
squirrel <squirrel@squirreldeMBP.lan>
2019-05-20 00:06:38 +0800

    123

--- client/App.jsx
+++ client/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { hot } from 'react-hot-loader' // eslint-disable-line
+import { hot } from 'react-hot-loader'
 
 // class App extends React.Component {
 //   render() {
